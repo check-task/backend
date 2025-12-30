fe6d64ab11d7af28df6bbbdf552fafba12be42d5
jungmin <kjmhs501@gmail.com>
2025-12-30 17:49:28 +0900

    chore: state 미들웨어 수정

--- src/middlewares/state.middleware.js
+++ src/middlewares/state.middleware.js
@@ -2,12 +2,5 @@ export const stateHandler = (req, res, next) => {
   res.success = (success) => {
     return res.json({ resultType: "SUCCESS", error: null, success });
   };
-  res.error = ({ errorCode = "unknown", reason = null, data = null }) => {
-    return res.json({
-      resultType: "FAIL",
-      error: { errorCode, reason, data },
-      success: null,
-    });
-  };
   next();
 };
