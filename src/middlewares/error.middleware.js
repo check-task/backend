e42e2e165108b7d423764f8c4e9ef5f29cc12feb
Kim Minji <128395453+songwol38@users.noreply.github.com>
2025-12-30 17:56:05 +0900

    에러 핸들러 수정

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,10 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-
-  res.status(err.statusCode || 500).error({
-    errorCode: err.errorCode || "unknown",
+  res.status(err.statusCode || 500).json({
+    errorCode: err.errorCode,
     reason: err.reason || err.message || null,
     data: err.data || null,
   });
