aba79415590843f4129a7a8cdf5659080414c97d
jungmin <kjmhs501@gmail.com>
2026-01-05 21:56:38 +0900

    feat: package.json에 prisma 명령어 등록

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,6 @@ import { defineConfig, env } from "@prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
 
-  // 새 엔진 대신 예전처럼 동작하는 classic 엔진 사용
-  // engine: "classic",
-
   // 데이터베이스 URL은 여기서 한 번만 설정
   datasource: {
     url: env("DATABASE_URL"),
