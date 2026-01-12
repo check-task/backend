647e4d1845f768aeaa08e1e7d012f0488b429697
jungmin <kjmhs501@gmail.com>
2026-01-13 00:32:53 +0900

    feat: 커스텀 에러 클래스 생성

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,7 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
   res.status(err.statusCode || 500).json({
-    errorCode: err.errorCode,
-    reason: err.reason || err.message || null,
+    errorCode: err.errorCode || "INTERNAL_SERVER_ERROR",
+    reason: err.reason || err.message || "서버 내부 오류가 발생했습니다",
     data: err.data || null,
   });
 };
