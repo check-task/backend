5eefe6f7212fbd89855df15c23288c6b3df45e48
jungmin <kjmhs501@gmail.com>
2026-01-13 00:46:32 +0900

    feat: 에러핸들러 수정

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ export const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
   res.status(statusCode).json({
-    resultType: "ERROR",
+    resultType: "FAIL",
     code: statusCode,
     errorCode: err.errorCode || "INTERNAL_SERVER_ERROR",
     reason: err.reason || err.message || "서버 내부 오류가 발생했습니다",
