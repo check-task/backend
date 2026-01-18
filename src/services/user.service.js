04b70f50385a14de4f7153b8c5f7e5ae4281ee7b
faker <seonjunu77@tukorea.ac.kr>
2026-01-18 20:47:45 +0900

    fix: 내 정보 조회 수정

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,8 @@
-import { NotFoundError } from "../errors/custom.error.js";
 import { responseFromUser } from "../dtos/user.dto.js";
-import { getUserData } from "../repositories/user.repository.js";
 
 class UserService {
-  async getMyInfo(userId) {
-    const user = await getUserData(userId);
-
-    if (!user) {
-      throw new NotFoundError("USER_NOT_FOUND", "해당 사용자를 찾을 수 없습니다.");
-    }
-    return responseFromUser(user);
+  async getMyInfo(userId){
+    return responseFromUser(userId);
   }
 }
 
