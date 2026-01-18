397aede1f4ecd4948253d4a536a21eca9b325e69
KIMMINJI <amber_lucy@naver.com>
2026-01-18 16:15:39 +0900

    feat : JWT에서 UerId 추출

--- src/routes/modal.route.js
+++ src/routes/modal.route.js
@@ -1,21 +1,22 @@
 import { Router } from 'express';
 import modalController from '../controllers/modal.controller.js';
+import authenticate from '../middlewares/authenticate.middleware.js';
 
 const router = Router();
 
 //자료
-router.post('/data/:taskId', modalController.createReferences);
-router.patch('/data/:taskId/:referenceId', modalController.updateReference);
-router.delete('/data/:taskId/:referenceId', modalController.deleteReference);
+router.post('/data/:taskId', authenticate, modalController.createReferences);
+router.patch('/data/:taskId/:referenceId', authenticate, modalController.updateReference);
+router.delete('/data/:taskId/:referenceId', authenticate, modalController.deleteReference);
 
 //커뮤니케이션
-router.post('/communication/:taskId', modalController.createCommunication);
-router.patch('/communication/:taskId/:communicationId', modalController.updateCommunication);
-router.delete('/communication/:taskId/:communicationId', modalController.deleteCommunication);
+router.post('/communication/:taskId', authenticate, modalController.createCommunication);
+router.patch('/communication/:taskId/:communicationId', authenticate, modalController.updateCommunication);
+router.delete('/communication/:taskId/:communicationId', authenticate, modalController.deleteCommunication);
 
 //회의록
-router.post('/log/:taskId', modalController.createLog);
-router.patch('/log/:taskId/:logId', modalController.updateLog);
-router.delete('/log/:taskId/:logId', modalController.deleteLog);
+router.post('/log/:taskId', authenticate, modalController.createLog);
+router.patch('/log/:taskId/:logId', authenticate, modalController.updateLog);
+router.delete('/log/:taskId/:logId', authenticate, modalController.deleteLog);
 
 export default router;
