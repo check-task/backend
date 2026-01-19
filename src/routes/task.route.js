4e3a024621a08da992b4be4a6b71dc8876529312
soyun0318 <thdbs0318@naver.com>
2026-01-19 13:26:07 +0900

    과제 API 로그인 미들웨어 연결

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,31 +1,23 @@
 import express from "express";
 import taskController from "../controllers/task.controller.js";
+import authenticate from '../middlewares/authenticate.middleware.js';
 
 const router = express.Router();
 
 // POST /api/v1/task -- 과제 생성
-router.post("/", taskController.createTask);
+router.post("/", authenticate, taskController.createTask);
 
 // PATCH /api/v1/task/:taskId -- 과제 수정
-router.patch("/:taskId", taskController.updateTask);
+router.patch("/:taskId", authenticate, taskController.updateTask);
 
 // DELETE /api/v1/task/:taskId -- 과제 삭제
-router.delete("/:taskId", taskController.deleteTask);
+router.delete("/:taskId", authenticate, taskController.deleteTask);
 
 // GET /api/v1/task/:taskId -- 과제 세부 사항 조회
-router.get("/:taskId", taskController.getTaskDetail);
+router.get("/:taskId", authenticate, taskController.getTaskDetail);
 
 // GET /api/v1/task?sort=우선순위 -- 과제 목록 조회
-router.get("/", taskController.getTasks);
-
-// GET /api/v1/task -- 과제 생성
-router.get("/", taskController.createTask);
-
-// PATCH /api/v1/task/:taskId
-router.patch("/:taskId", taskController.updateTask);
-
-// DELETE /api/v1/task/:taskId
-router.delete("/:taskId", taskController.deleteTask);
+router.get("/", authenticate, taskController.getTasks);
 
 // 세부 TASK 완료 처리 API 
 // 세부 TASK 상태 업데이트
