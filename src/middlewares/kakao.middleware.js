c60c4376e456965becd5ec52d9c140db5671a742
KIMMINJI <amber_lucy@naver.com>
2026-01-28 17:27:02 +0900

    fix : passport 동기 처리

--- src/middlewares/kakao.middleware.js
+++ src/middlewares/kakao.middleware.js
@@ -2,10 +2,12 @@ import passport from "../config/passport.config.js";
 
 export const kakaoMiddleware = {
   // 카카오 로그인 시작
-  start: passport.authenticate("kakao", {
-    session: false,
-    callbackURL: process.env.KAKAO_CALLBACK_URL
-  }),
+  start: (req, res, next) => {
+    passport.authenticate("kakao", {
+      session: false,
+      callbackURL: process.env.KAKAO_CALLBACK_URL,
+    })(req, res, next);
+  },
 
   // 카카오 콜백
   callback: passport.authenticate("kakao", {
