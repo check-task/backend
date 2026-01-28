7f05442668f2630573289143e18fa0bf5b567033
KIMMINJI <amber_lucy@naver.com>
2026-01-28 18:26:45 +0900

    fix : 콜백url 명시

--- src/middlewares/kakao.middleware.js
+++ src/middlewares/kakao.middleware.js
@@ -1,10 +1,13 @@
 import passport from "../config/passport.config.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const kakaoMiddleware = {
   // 카카오 로그인 시작
   start: (req, res, next) => {
     passport.authenticate("kakao", {
       session: false,
+      callbackURL: process.env.KAKAO_CALLBACK_URL,
     })(req, res, next);
   },
 
