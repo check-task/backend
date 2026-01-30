8b4b1674525051d3313daf099b65269c819fa5c8
KIMMINJI <amber_lucy@naver.com>
2026-01-30 15:27:21 +0900

    fix : 세션 허용

--- src/middlewares/kakao.middleware.js
+++ src/middlewares/kakao.middleware.js
@@ -6,14 +6,12 @@ export const kakaoMiddleware = {
   // 카카오 로그인 시작
   start: (req, res, next) => {
     passport.authenticate("kakao", {
-      session: false,
       callbackURL: process.env.KAKAO_CALLBACK_URL,
     })(req, res, next);
   },
 
   // 카카오 콜백
   callback: passport.authenticate("kakao", {
-    session: false,
     failureRedirect: "/login-failed",
   }),
 };
