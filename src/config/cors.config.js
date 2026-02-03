e0edc1ad9a9cd0d9201635cf0bde72d432658b07
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-02-03 17:52:02 +0900

    [Fix/#164]로그인 관련 버그 수정

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,6 +1,7 @@
 export const corsOptions = {
   origin: [
     "http://localhost:3000",
+    "http://localhost:8000",
     "https://frontend-one-chi-11.vercel.app",
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
